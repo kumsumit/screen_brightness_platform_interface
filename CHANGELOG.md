886dbfab235a94b0afa5af4f51577d3bb90511e3
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-09-19 18:27:49 +0530

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+## 1.0.1
+
+* added animate boolean
+
+## 1.0.0
+
+* updated minimum supported SDK version to Flutter 3.0/Dart 3.0.
+
+## 0.1.0
+
+* changed dependencies version using version range
+
+## 0.0.4
+
+* added auto reset boolean
+* downgrade plugin_platform_interface to support variety of version
+
+## 0.0.3
+
+* added has change boolean
+
+## 0.0.2
+
+* fixed broken repository link in pubspec
+
+## 0.0.1
+
+* initial release
---
+++ lib/constant/method_name.dart
@@ -0,0 +1,12 @@
+const methodNameGetSystemScreenBrightness = 'getSystemScreenBrightness';
+const methodNameGetScreenBrightness = 'getScreenBrightness';
+const methodNameSetScreenBrightness = 'setScreenBrightness';
+const methodNameResetScreenBrightness = 'resetScreenBrightness';
+
+const methodNameHasChanged = 'hasChanged';
+
+const methodNameIsAutoReset = 'isAutoReset';
+const methodNameSetAutoReset = 'setAutoReset';
+
+const methodNameIsAnimate = 'isAnimate';
+const methodNameSetAnimate = 'setAnimate';
